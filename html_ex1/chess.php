0f19c1fa81e0ccce3c3a67b2f2f02b811bfd0fcd
Marjorie Amaya <fdc.marj@gmail.com>
2022-03-24 17:02:30 +0800

    Add file for Chess board activity

---
+++ html_ex1/chess.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chess</title>
+</head>
+<style>
+    .board{
+        width: 700px;
+        height: 700px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    table, tr, td{
+        border: 1px solid;
+    }
+    .black {
+        background-color: #a6a6a6;
+    }
+    .white{
+        background-color: #f2f2f2;
+    }
+    td { 
+        width: 1.5em; 
+        height: 1.5em; 
+        text-align: center; 
+        font-size: 50px;
+    }
+    tr:nth-child(even) {background: red}
+
+</style>
+<body>
+    <table class="board">
+        <!-- 8 White first -->
+        <tr>
+            <div>
+                <td class="white">&#9820;</td>
+                <td class="black">&#9822;</td>
+                <td class="white">&#9821;</td>
+                <td class="black">&#9819;</td>
+                <td class="white">&#9818;</td>
+                <td class="black">&#9821;</td>
+                <td class="white">&#9822;</td>
+                <td class="black">&#9820;</td>
+            </div>
+        </tr>
+       
+        <!--7 Black first -->
+        <tr>
+            <div>
+                <td class="black">&#9821;</td>
+                <td class="white">&#9821;</td>
+                <td class="black">&#9821;</td>
+                <td class="white">&#9821;</td>
+                <td class="black">&#9821;</td>
+                <td class="white">&#9821;</td>
+                <td class="black">&#9821;</td>
+                <td class="white">&#9821;</td>
+            </div>
+        </tr>
+         <!-- 6 White first -->
+         <tr>
+             <div>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+            </div>
+        </tr>
+        <!-- 5 Black first -->
+        <tr>
+            <div>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+            </div>
+        </tr>
+         <!-- 4 White first -->
+         <tr>
+            <div>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+            </div>
+        </tr>
+        <!-- 3 Black first -->
+        <tr>
+            <div>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+                <td class="black"></td>
+                <td class="white"></td>
+            </div>
+        </tr>
+          <!-- 2 White first -->
+          <tr>
+            <div>
+                <td class="white">&#9817;</td>
+                <td class="black">&#9817;</td>
+                <td class="white">&#9817;</td>
+                <td class="black">&#9817;</td>
+                <td class="white">&#9817;</td>
+                <td class="black">&#9817;</td>
+                <td class="white">&#9817;</td>
+                <td class="black">&#9817;</td>
+            </div>
+        </tr>
+        <!-- 1 Black first -->
+        <tr>
+            <div>
+                <td class="black">&#9814;</td>
+                <td class="white">&#9816;</td>
+                <td class="black">&#9815;</td>
+                <td class="white">&#9813;</td>
+                <td class="black">&#9812;</td>
+                <td class="white">&#9815;</td>
+                <td class="black">&#9816;</td>
+                <td class="white">&#9814;</td>
+            </div>
+        </tr>
+           
+    </table>
+</body>
+</html>
